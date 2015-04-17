aa587c17d38eb7bea53070f1b0eba330cdedc36e
neco- <neco.vim@gmail.com>
2015-04-17 17:31:05 +0900

    added message for Cygwin.
    Change-Id: Id981021fc420b5ee927fd683555cccf1da455d5f

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     bash setup_ubuntu.sh
 elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
     # Cygwin
+    echo "not implement yet."
 else
     echo "Your platform ($(uname -a)) is not supported."
     exit 1
