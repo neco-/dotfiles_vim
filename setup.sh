e4d95b2853057213d680422142a37ac70f472ecf
neco- <neco.vim@gmail.com>
2015-04-21 11:09:43 +0900

    fixed setup scripts.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-    bash setup_ubuntu.sh
+    # Linux
+    bash setup/setup_linux.sh
 elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
     # Cygwin
-    echo "not implement yet."
+    bash setup/setup_cygwin.sh
 else
     echo "Your platform ($(uname -a)) is not supported."
-    exit 1
 fi
